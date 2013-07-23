9c179a2e24ee7b151e4d2a6686d4c2b5231df93e
Roopesh Shenoy <shenoy.roopesh@gmail.com>
2013-07-24 03:21:37 +0530

    Adding a separate invoker project so that our starting point is an exe

---
+++ Invoker/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Invoker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proc = new Process();
+            proc.StartInfo.FileName = "runGCompris.bat";
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.UseShellExecute = false;
+            proc.Start();
+            proc.WaitForExit();
+        }
+    }
+}
